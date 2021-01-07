bd68e56a1a4adaafb89d1dcd2b3553459763417e
Jonas Rodrigues <jonas.rodrigues@unisoma.com>
2021-01-07 18:31:32 -0300

    Adiciona o básico do Flask Admin

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 from flask import Flask
+from flask_admin import Admin
+from dynaconf import FlaskDynaconf
 
 app = Flask(__name__)
 
+# automatic settings
+FlaskDynaconf(app)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
-
+admin = Admin(
+    app,
+    name='Eufraten',
+    template_mode='bootstrap3',
+)
 
 if __name__ == '__main__':
     app.run()
